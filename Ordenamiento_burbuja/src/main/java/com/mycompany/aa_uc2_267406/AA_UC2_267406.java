0cc186bbf15ad7d8510199f2222ed5a426afb938
Jesus Moreno <jesus.moreno267406@potros.itson.edu.mx>
2026-03-10 15:29:27 -0700

    Add files via upload

---
+++ Ordenamiento_burbuja/src/main/java/com/mycompany/aa_uc2_267406/AA_UC2_267406.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.aa_uc2_267406;
+
+/**
+ *
+ * @author kingt
+ */
+public class AA_UC2_267406 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
