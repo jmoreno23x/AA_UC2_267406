2d78d7fe9cfc574a23dc0bd3fed51506bd524a04
Jesus Moreno <jesus.moreno267406@potros.itson.edu.mx>
2026-03-17 09:40:02 -0700

    Delete Ordenamientos/Ordenamiento-insercion directory

--- Ordenamientos/Ordenamiento-insercion/Busquedas.java
+++
@@ -1,8 +0,0 @@
-/**
- * Clase que contendrá los métodos de búsqueda.
- * @author [Nombre del alumno]
- * @version 1.0
- */
-public class Busquedas {
-    // Métodos de búsqueda se agregarán en actividades posteriores
-}
